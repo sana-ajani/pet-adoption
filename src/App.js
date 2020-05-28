d4253ba5d61fbca8b4fdaa7ef90c23583a1685a0
sana-ajani <sajani6@gatech.edu>
2020-05-28 16:21:09 -0400

    add app and pet components

---
+++ src/App.js
@@ -0,0 +1,32 @@
+//creating the rubber stamp
+const Pet = ({ name, animal, breed }) => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, name),
+    React.createElement("h2", {}, animal),
+    React.createElement("h2", {}, breed),
+  ]);
+};
+
+//stamping it 3 times
+const App = () => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Adopt me!"),
+    React.createElement(Pet, {
+      name: "Charlie",
+      animal: "Dog",
+      breed: "Havanese",
+    }),
+    React.createElement(Pet, {
+      name: "Romeo",
+      animal: "Dog",
+      breed: "Labrador",
+    }),
+    React.createElement(Pet, {
+      name: "Gracie",
+      animal: "Cat",
+      breed: "Mixed",
+    }),
+  ]);
+};
+
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
