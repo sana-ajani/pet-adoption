89f09909ca5ca2ea076a0ee6459dc4ccfe111203
sana-ajani <sajani6@gatech.edu>
2021-09-01 15:09:38 -0400

    update

--- src/App.js
+++ src/App.js
@@ -1,16 +1,7 @@
 import React from "react";
 import { render } from "react-dom";
+import Pet from "./Pet";
 
-//creating the rubber stamp
-const Pet = ({ name, animal, breed }) => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, name),
-    React.createElement("h2", {}, animal),
-    React.createElement("h2", {}, breed),
-  ]);
-};
-
-//stamping it 3 times
 const App = () => {
   return React.createElement("div", {}, [
     React.createElement("h1", {}, "Adopt me!"),
